67b722958b5cf59c2f393dda257e5ceeb69a514a
thinh <daothinh1105@gmail.com>
2023-02-27 18:10:29 +0700

    quan li user khi dang nhap va set status

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,10 @@ const router = express.Router();
 // Đăng nhập
 
 router.post("/login", authController.login);
-
+router.get("/getUser",authController.getData)
+//http://localhost:3000/login
+router.put("/setUserStatus/:userId", authController.setUserStatus);
+//example http://localhost:3000/setUserStatus/63fc61ef617136555e017603
+router.delete("/deleteUser/:userId",authController.deleteUser)
+//example http://localhost:3000/deleteUser/63fc6149d0db61d24a614b62
 module.exports = router;
