16b59407f570dd4a181d6b39ea4b84df0efd87ac
thinh <daothinh1105@gmail.com>
2023-03-04 20:44:13 +0700

    config authen 1

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,13 +1,23 @@
+const admin = require("firebase-admin");
 
-const admin = require("../config/serviceAccount.json");
+// Initialize Firebase Admin SDK only if there are no apps already initialized
+if (!admin.apps.length) {
+  const serviceAccount = require("../config/serviceAccount.json");
 
-exports.authenticate = async (req, res, next) => {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const authenticate = async (req, res, next) => {
   try {
+    // Get the ID token from the request's authorization header
     const idToken = req.headers.authorization.split("Bearer ")[1];
-
+    // Verify the ID token
     const decodedToken = await admin.auth().verifyIdToken(idToken);
+    // Add the user's ID to req.user
     req.user = decodedToken;
-    console.log(decodedToken)
+    console.log(decodedToken);
     next();
   } catch (err) {
     console.error(err);
@@ -15,9 +25,4 @@ exports.authenticate = async (req, res, next) => {
   }
 };
 
-// module.exports = authenticate;
-
-
-
-
-
+module.exports = authenticate;
