50743165d3eebbb96fac76c5ef90350e87ed52df
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2023-03-10 04:21:42 +0700

    Update room.js

--- models/room.js
+++ models/room.js
@@ -6,6 +6,7 @@ const roomSchema = new mongoose.Schema(
         price: { type: Number, min: 0 },
         description: { type: String },
         img: { type: String },
+        roomNo: { type: String, default:"0"},
         status: { type: Boolean, default: true },
         buildings: { type: mongoose.Schema.Types.ObjectId, ref: "Building" },
         users: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
