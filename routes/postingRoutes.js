3df3f444055f75e0b14bd1993e6a13f807e59a05
Vũ Tài <92210830+taivt21@users.noreply.github.com>
2023-03-16 15:16:02 +0700

    Update postingRoutes.js

--- routes/postingRoutes.js
+++ routes/postingRoutes.js
@@ -4,12 +4,18 @@ const authenticate = require("../middlewares/authenticate");
 const uploadImage = require("../middlewares/uploadImage");
 const router = express.Router();
 
-router.post('/postingNew', authenticate, postingController.createPosting)
+router.post('/createPosting', authenticate, uploadImage, postingController.createPosting)
+
 router.get('/getAllPostings',postingController.getAllPostings)
+
 router.get('/getPostingByUserId', authenticate, postingController.getAllPostings)
+
 router.get('/getPostingById/:id',postingController.getPostingById)
+
 router.put('/updatePosting/:id',postingController.updatePosting)
+
+router.put('/setStatusPosting/:id', postingController.setStatusPosting)
+
 router.delete('/deletePosting/:id',postingController.deletePosting)
 
-router.get('/getAllPostingByUserId',authenticate, postingController.getPostingByUserId)
 module.exports = router;
