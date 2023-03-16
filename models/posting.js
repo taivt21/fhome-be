64b3df6b5be6c547bc7119e1976d7bff0ee5875f
Vinh Tran <53442440+tranvinh99@users.noreply.github.com>
2023-03-17 00:30:57 +0700

    paypal thanh toan

--- models/posting.js
+++ models/posting.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
-    {
-        title: { type: String },
-        description: { type: String },
-        status: { type: Boolean, default: true },
-        buildings: { type: mongoose.Schema.Types.ObjectId, ref: "Buildings" },
-        rooms: { type: mongoose.Schema.Types.ObjectId, ref: "Rooms" },
-        userPosting: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-        img: { type: String },
-    },
-    { timestamps: true }
+  {
+    title: { type: String },
+    description: { type: String },
+    status: { type: Boolean, default: true },
+    buildings: { type: mongoose.Schema.Types.ObjectId, ref: "Buildings" },
+    rooms: { type: mongoose.Schema.Types.ObjectId, ref: "Rooms" },
+    userPosting: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+    img: { type: String },
+  },
+  { timestamps: true }
 );
 
 const Postings = mongoose.model("Postings", postSchema);
