5f8483194f1951a2eac62555d6a56326d7f9dee3
Tai <taivutan218@gmail.com>
2023-03-18 02:59:38 +0700

    push

--- routes/notiRoutes.js
+++ routes/notiRoutes.js
@@ -1,8 +1,8 @@
-const pushNoti = require("../controllers/pushNotification");
+// const pushNoti = require("../controllers/pushNotification");
 const authenticate = require("../middlewares/authenticate");
 const express = require('express');
 const router = express.Router();
 
-router.post("/pushNoti", authenticate, pushNoti.sendNotification)
+// router.post("/pushNoti", authenticate, pushNoti.sendNotification)
 
 module.exports = router
